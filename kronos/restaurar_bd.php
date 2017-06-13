80eeaf264d06fc20f02b328d9ef6bf0f945fce6f
Unknown <enlinea@gmail.com>
2017-06-12 23:56:34 -0300

    type error

--- kronos/restaurar_bd.php
+++ kronos/restaurar_bd.php
@@ -2,7 +2,7 @@
 
 //Esto no se ejecuta porque PHP debería estar en modo inseguro para que funcione.
 //Por lo tanto, debe tomarse el contenido de la variable $comando y ejecutarse por linea de comandos.
-$comando = 'mysql --user=root --password kronos2 < dump_kronos.sql';
+$comando = 'mysql --user=root --password kronos < dump_kronos.sql';
 
 system($comando, $error);
 
